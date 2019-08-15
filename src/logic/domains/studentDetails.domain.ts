9f2676675d0450e884edba33a4d1fe79e093e04c
Sebastian Southern <sebastian.southern@gmail.com>
2019-08-15 22:25:57 +1000

    adding types to registration form

---
+++ src/logic/domains/studentDetails.domain.ts
@@ -0,0 +1,24 @@
+
+export interface StudentDetails {
+    [title: string]: string;
+    fullName: string;
+    preferredName: string;
+    faculty: string;
+    courseID: string;
+    email: string;
+    preferredContactNumber: string;
+    dateOfBirth: string;
+    gender: string;
+    degree: string;
+    status: string;
+}
+
+export interface EducationalBackground {
+    [course: string]: Course;
+}
+
+export class Course {
+    title = '';
+    mark = 0;
+    isChecked = false;
+}
