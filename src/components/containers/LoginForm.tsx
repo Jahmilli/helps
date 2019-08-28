f0faf090174add6b9add62f3d028620ab651fe54
Sebastian Southern <sebastian.southern@gmail.com>
2019-08-29 08:58:28 +1000

    refactored moving all formik stuff to login page

--- src/components/containers/LoginForm.tsx
+++ src/components/containers/LoginForm.tsx
@@ -1,90 +1,41 @@
-import React, { useState } from 'react';
-import { Button, Typography, FormControl, InputLabel, Input, FormHelperText, IconButton, TextField } from '@material-ui/core';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import { Close as CloseIcon } from '@material-ui/icons';
+import React from 'react';
+import { Button, Typography, TextField } from '@material-ui/core';
 import Auth from '../../logic/functions/core/Auth';
-import { loginFormStyles } from './styles';
 import logo from '../../images/uts-logo.png';
-import MySnackbarContentWrapper from '../presentational/SnackBarContentWrapper';
 
 interface LoginFormProps {
     auth: Auth;
+    classes: any;
+    formikProps: any;
 }
 
-const LoginForm: React.FunctionComponent<LoginFormProps> = ({ auth }) => {
-    const initialUserDetailsState = {
-        email: '',
-        password: ''
-    };
-    
-    const handleSubmit = async (values: any) => {    
-        console.log('values are ', values);
-        try {
-            await auth.login(values.email, values.password);
-        } catch (err) {
-            alert('Login failed, please try again');
-        }
-    }
-
-    const SignupSchema = Yup.object().shape({
-        email: Yup.string()
-            .email('Invalid email')
-            .required('Student Email Required'),
-        password: Yup.string()
-            .required('Password Required')
-    });
-    
-    
-    const classes = loginFormStyles();
+const LoginForm: React.FunctionComponent<LoginFormProps> = ({ auth, classes, formikProps }) => {
     return (
-        <div style={{ display: 'flex', justifyContent: 'center'}}>
-            <div className={classes.gridLockup}>
-                <Formik
-                initialValues={initialUserDetailsState}
-                validationSchema={SignupSchema}
-                onSubmit={(values: any) => handleSubmit(values)}
-                >
-                {(props) => {
-                const {
-                    values,
-                    touched,
-                    errors,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                } = props;
-                return (
-                    <form onSubmit={handleSubmit}>
-                        <img src={logo} className={classes.logo} alt="UTS Logo" />
-                        <Typography variant="h1" style={{fontSize: 32, fontWeight: 'bold', color: 'black', textAlign:'start'}}>Log In</Typography>
-                        <TextField
-                            className={classes.textfield}
-                            label="email"
-                            name="email"
-                            value={values.email}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            helperText={(errors.email && touched.email) && errors.email}
-                        />
-                        <TextField
-                            className={classes.textfield}
-                            label="password"
-                            name="password"
-                            type="password"
-                            value={values.password}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            helperText={(errors.password && touched.password) && errors.password}
-                            // margin="normal"
-                        />
-                        {/* <ErrorMessage name="password" component="div" className={classes.fieldError}></ErrorMessage> */}
-                        <button type="submit" className={classes.submitBtn}>Submit</button>
-                    </form>
-                )}}
-                </Formik>
-            </div>
-        </div>
+        <form onSubmit={formikProps.handleSubmit}>
+            <img src={logo} className={classes.logo} alt="UTS Logo" />
+            <Typography variant="h1" style={{fontSize: 32, fontWeight: 'bold', color: 'black', textAlign:'start'}}>Log In</Typography>
+            <TextField
+                className={classes.textfield}
+                label="email"
+                name="email"
+                value={formikProps.values.email}
+                onChange={formikProps.handleChange}
+                onBlur={formikProps.handleBlur}
+                helperText={(formikProps.errors.email && formikProps.touched.email) && formikProps.errors.email}
+            />
+            <TextField
+                className={classes.textfield}
+                label="password"
+                name="password"
+                type="password"
+                value={formikProps.values.password}
+                onChange={formikProps.handleChange}
+                onBlur={formikProps.handleBlur}
+                helperText={(formikProps.errors.password && formikProps.touched.password) && formikProps.errors.password}
+            />
+            {/* <ErrorMessage name="password" component="div" className={classes.fieldError}></ErrorMessage> */}
+            <button type="submit" className={classes.submitBtn}>Submit</button>
+        </form>
     );
 };
 
--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -3,6 +3,9 @@ import { getTestData } from '../../logic/functions/testFetch';
 import LoginForm from '../containers/LoginForm';
 import './styles.css';
 import Auth from '../../logic/functions/core/Auth';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
+import { loginFormStyles } from '../containers/styles';
 
 interface LoginPageProps {
     auth: Auth;
@@ -18,9 +21,45 @@ const LoginPage: React.FunctionComponent<LoginPageProps> = ({auth}) => {
         callGetTest();
     }, []);
 
+    const initialUserDetailsState = {
+        email: '',
+        password: ''
+    };
+    
+    const handleSubmit = async (values: any) => {    
+        console.log('values are ', values);
+        try {
+            await auth.login(values.email, values.password);
+        } catch (err) {
+            alert('Login failed, please try again');
+        }
+    }
+
+    const SignupSchema = Yup.object().shape({
+        email: Yup.string()
+            .email('Invalid email')
+            .required('Student Email Required'),
+        password: Yup.string()
+            .required('Password Required')
+    });
+
+    const classes = loginFormStyles();
+
     return (
         <div className="loginPage">
-            <LoginForm auth={auth} />
+            <div style={{ display: 'flex', justifyContent: 'center'}}>
+                <div className={classes.gridLockup}>
+                    <Formik
+                    initialValues={initialUserDetailsState}
+                    validationSchema={SignupSchema}
+                    onSubmit={(values: any) => handleSubmit(values)}
+                    >
+                        {(props) => { 
+                            return <LoginForm auth={auth} classes={classes} formikProps={props} />
+                        }}
+                   </Formik>
+                </div>
+            </div>
         </div>
     );
 };
