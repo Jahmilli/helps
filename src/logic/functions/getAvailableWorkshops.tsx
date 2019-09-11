644b74a1900c73d6c68bf1e4dd2d9c7623b47a2b
Luke Phillips <lukephillips9216@gmail.com>
2019-09-11 20:24:03 +1000

    working on saving payload

--- src/logic/functions/getAvailableWorkshops.tsx
+++ src/logic/functions/getAvailableWorkshops.tsx
@@ -1,9 +1,14 @@
-import { get } from './core/fetch';
+import { get } from "./core/fetch";
+import { WorkshopDetails } from "../domains/workshopDetails.domain";
 
 export const getCurrentWorkshops = async (): Promise<any> => {
-    return await get('/api/v1/workshop/current');
-}
+	return await get("/api/v1/workshop/current");
+};
 
 export const getArchivedWorkshops = async (): Promise<any> => {
-    return await get('/api/v1/workshop/archived');
-}
+	return await get("/api/v1/workshop/archived");
+};
+
+export const postWorkshop = async (workshop: WorkshopDetails) => {
+	return await post("/api/v1/workshop/archived", JSON.stringify(workshop));
+};
