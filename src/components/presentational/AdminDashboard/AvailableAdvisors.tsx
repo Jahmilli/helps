e88727220442a64d9c4261fa3f63bb3f5be528e5
Ethanol10 <35375774+Ethanol10@users.noreply.github.com>
2019-10-02 22:34:37 +1000

    added add Advisor text boxes

--- src/components/presentational/AdminDashboard/AvailableAdvisors.tsx
+++ src/components/presentational/AdminDashboard/AvailableAdvisors.tsx
@@ -7,6 +7,8 @@ import { SvgIconProps } from '@material-ui/core/SvgIcon';
 import { withRouter } from 'react-router-dom';
 import { RouteComponentProps } from 'react-router';
 import EnhancedTable from '../SelectableTable'
+import TextField from '@material-ui/core/TextField';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
 const icons = {
     Add: () => <Add /> as React.ReactElement<SvgIconProps>
@@ -17,8 +19,40 @@ type AvailableSessionsProps = RouteComponentProps<any> & {
     isAdmin: boolean;
 }
 
-const AvailableAdvisors: React.FunctionComponent<AvailableSessionsProps> = (props) => {
+interface State {
+    name: string;
+  }
+  
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 200,
+    },
+    dense: {
+      marginTop: 19,
+    },
+    menu: {
+      width: 200,
+    },
+  }),
+);
 
+const AvailableAdvisors: React.FunctionComponent<AvailableSessionsProps> = (props) => {
+    const classes = useStyles();
+    const [values, setValues] = React.useState<State>({
+      name: ''
+    });
+  
+    const handleChange = (name: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValues({ ...values, [name]: event.target.value });
+    };
+  
     return (
         <div>
             <EnhancedTable tableTitle = "Available Advisors" rows = {[]}/>
@@ -34,6 +68,45 @@ const AvailableAdvisors: React.FunctionComponent<AvailableSessionsProps> = (prop
                 <Button id="delete" color="primary" size="large" >Delete</Button>
                 <Button id="update" color="primary" size="large" >Update</Button>
                 <Button id="inactive" color="primary" size="large" >Inactive</Button>
+            </div>
+
+            <Typography variant="h6">
+                    Add new Advisor
+            </Typography>
+            <div style={{display: 'flex', justifyContent:'center', alignItems:'center'}}>   
+                <TextField
+                    id="name"
+                    label="Name"
+                    className={classes.textField}
+                    defaultValue={""}
+                    onChange={handleChange('name')}
+                    margin="normal"
+                />
+                <TextField
+                    id="first-name"
+                    label="First Name"
+                    className={classes.textField}
+                    defaultValue={""}
+                    onChange={handleChange('name')}
+                    margin="normal"
+                />
+                <TextField
+                    id="last-name"
+                    label="Last Name"
+                    className={classes.textField}
+                    defaultValue={""}
+                    onChange={handleChange('name')}
+                    margin="normal"
+                />
+                <TextField
+                    id="email"
+                    label="Email"
+                    className={classes.textField}
+                    defaultValue={""}
+                    onChange={handleChange('name')}
+                    margin="normal"
+                />
+                <Button id="addAdvisor" color="primary" size="large" >Add Advisor</Button>
             </div>
         </div>
     );
